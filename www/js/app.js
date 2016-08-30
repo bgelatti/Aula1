969301c4af51a530f7fdf87bdd7a7d87d544dff3
Bruno Gelatti <bgelatti@gmail.com>
2016-08-29 22:21:47 -0300

    adicionado date picker

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,26 @@
 (function() {
 
-var app = angular.module('bloconotas', ['ionic', 'bloconotas.banconota']);
+var app = angular.module('bloconotas', ['ionic', 'bloconotas.banconota', 'ionic-datepicker']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider, ionicDatePickerProvider) {
+
+  var datePickerObj = {
+      inputDate: new Date(),
+      setLabel: 'Set',
+      todayLabel: 'Today',
+      closeLabel: 'Close',
+      mondayFirst: false,
+      weeksList: ["S", "M", "T", "W", "T", "F", "S"],
+      monthsList: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+      templateType: 'popup',
+      from: new Date(2012, 8, 1),
+      to: new Date(2018, 8, 1),
+      showTodayButton: true,
+      dateFormat: 'dd MMMM yyyy',
+      closeOnSelect: false,
+      disableWeekdays: [6]
+    };
+    ionicDatePickerProvider.configDatePicker(datePickerObj);
 
   $stateProvider.state('lista', {
     url: '/lista',
@@ -43,7 +61,7 @@ app.controller('listaControle', function($scope, BancoNota) {
 
 });
 
-app.controller('novoControle', function($scope, $state, BancoNota) {
+app.controller('novoControle', function($scope, $state, BancoNota, ionicDatePicker) {
 
   $scope.note = {
     id: new Date().getTime().toString(),
@@ -55,6 +73,32 @@ app.controller('novoControle', function($scope, $state, BancoNota) {
     BancoNota.create($scope.note);
     $state.go('lista');
   };
+
+  var ipObj1 = {
+      callback: function (val) {  //Mandatory
+        console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+      },
+      disabledDates: [            //Optional
+        new Date(2016, 2, 16),
+        new Date(2015, 3, 16),
+        new Date(2015, 4, 16),
+        new Date(2015, 5, 16),
+        new Date('Wednesday, August 12, 2015'),
+        new Date("08-16-2016"),
+        new Date(1439676000000)
+      ],
+      from: new Date(2012, 1, 1), //Optional
+      to: new Date(2016, 10, 30), //Optional
+      inputDate: new Date(),      //Optional
+      mondayFirst: true,          //Optional
+      disableWeekdays: [0],       //Optional
+      closeOnSelect: false,       //Optional
+      templateType: 'popup'       //Optional
+    };
+
+    $scope.openDatePicker = function(){
+      ionicDatePicker.openDatePicker(ipObj1);
+    };
 });
 
 app.controller('editarControle', function($scope, $state, BancoNota) {
